01c975fb4cf5a42bab226c512e3479f3bfd47188
cjhubbs <chris@chrishubbs.com>
2026-04-14 18:57:43 -0500

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://cjhubbs.github.io",
-  base: "/astro-books",
+  site: "https://booklog.chrishubbs.com",
   output: "static",
 });
